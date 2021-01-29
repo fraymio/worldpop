260e6075fdb491ac1a1394567723ee133822244b
lmcindewar <l.mcindewar@fraym.io>
2021-01-29 10:58:52 -0500

    updating insert functions with iso3 and tile number, moving metadata parse to insert

--- worldpop/insert.py
+++ worldpop/insert.py
@@ -12,28 +12,12 @@ S3_BUCKET = "fraym-worldpop"
 
 
 def insert(
-    file: str,
-    year: int,
-    gender: str,
-    age_lower: int,
-    age_upper: Optional[int] = None,
-    tile_size: int = 400,
-    dev: bool = False,
+    file: str, tile_size: int = 400, dev: bool = False,
 ):
     """ Insert a World Pop gender/age population raster into database.
 
     :param file name of raster file
     :type str
-
-    :param year year of raster data
-    :type int
-    :param gender either 'm' or 'f'
-    :type str
-    :param age_lower lower age of population
-    :type int
-    :param age_upper upper age of population
-    :type int
-
     :param tile_size number of pixels in tile, eg. 400 creates 400x400 tiles
     :type int
     :param dev insert into dev or live database
@@ -41,11 +25,22 @@ def insert(
 
     :rtype None, raster is inserted
     """
+    iso3_code, gender, age_lower, age_upper, year = worldpop_metadata(file)
+
+    if age_upper == 0:
+        # For the 80 and up group, set upper bound to missing
+        age_upper = None
+
+    wp_data = dict(
+        iso3_code=iso3_code,
+        year=year,
+        gender=gender.lower(),
+        age_lower=age_lower,
+        age_upper=age_upper,
+    )
     db_session = create_session(dev=dev)
-    for tile in raster2pgsql(file, tile_size=tile_size):
-        wp_tile = WorldPop(
-            year=year, gender=gender.lower(), age_lower=age_lower, age_upper=age_upper,
-        )
+    for tile_number, tile in enumerate(raster2pgsql(file, tile_size=tile_size)):
+        wp_tile = WorldPop(**wp_data, tile_number=tile_number + 1)
         wp_tile.rast = standardize_tile(tile, dev=dev)
         db_session.add(wp_tile)
         try:
@@ -75,21 +70,10 @@ def insert_from_s3(iso3_code: str, year: int, tile_size: int = 400, dev: bool = 
     """
 
     s3 = boto3.resource("s3")
-    pattern = re.compile(r"^[0-9]{4}/[a-z]{3}/.*.tif$")
-    for obj in s3.Bucket(S3_BUCKET).objects.iterator():
-        if not pattern.findall(obj.key):
-            continue
+    for obj in s3.Bucket(S3_BUCKET).objects.filter(f"{year}/{iso3_code.lower()}"):
         local = os.path.basename(obj.key)
-        file_iso3, gender, age_lower, age_upper, file_year = worldpop_metadata(local)
-        if int(file_year) != year or file_iso3.upper() != iso3_code.upper():
-            continue
-        if age_upper == 0:
-            # For the 80 and up group, set upper bound to missing
-            age_upper = None
         s3.meta.client.download_file(S3_BUCKET, obj.key, local)
-        insert(
-            local, year, gender, age_lower, age_upper, tile_size=tile_size, dev=dev,
-        )
+        insert(local, tile_size=tile_size, dev=dev)
         try:
             os.remove(local)
         except (FileNotFoundError, PermissionError):
